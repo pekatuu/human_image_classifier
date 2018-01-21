d6fdbd8d169be7d3c2125d063ecf59b41406fa38
pekatuu <pekatuu@gmail.com>
2018-01-21 21:27:58 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,89 @@
+import argparse
+import io
+import os
+
+import flask_admin
+from flask import Flask, send_file, jsonify
+from flask_admin.contrib.peewee import ModelView
+
+import sqlite_adapter as dba
+
+DEFAULT_DB_NAME = "hic.db"
+
+app = Flask("human image classifier")
+app.config['SECRET_KEY'] = "extremely super safe secret key"
+
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("--ip", type=str, required=True)
+    p.add_argument("--port", type=int, required=True)
+
+    p.add_argument("--init", action="store_true")
+    p.add_argument("--force", action="store_true")
+    p.add_argument("--image-root", type=str)
+    p.add_argument("--image-exts", type=str, default=".png,.jpg,.jpeg,.bmp,.gif")
+    p.add_argument("--tags", type=str, required=False)
+    p.add_argument("--db-path", type=str, required=False)
+
+    p.add_argument("--debug", action="store_true")
+
+    return p.parse_args()
+
+
+def init_flask_admin():
+    admin = flask_admin.Admin(app)
+    admin.add_view(ModelView(dba.DatasetInfo))
+    admin.add_view(ModelView(dba.Image))
+    admin.add_view(ModelView(dba.Tag))
+    admin.add_view(ModelView(dba.ImageToTag))
+
+
+@app.route("/image/<int:image_id>")
+def get_image(image_id: int):
+    image_name = db.get_image_name(image_id)
+    image_ext = os.path.splitext(image_name)[1]
+    image_path = os.path.join(db.get_dataset_info().image_root, image_name)
+    with open(image_path, 'rb') as f:
+        return send_file(io.BytesIO(f.read()),
+                         attachment_filename=image_name,
+                         mimetype=f'image/{image_ext[1:]}')
+
+
+@app.route("/image/<int:image_id>/tag", methods=["GET"])
+def get_tag(image_id):
+    return jsonify(f"{image_id}")
+
+
+@app.route("/image/<int:image_id>/tag/<string:tag_name>", methods=["POST"])
+def add_tag(image_id: int, tag_name: str):
+    tag = db.get_tag_by_name(tag_name)
+    db.add_tag_to_image(image_id, tag)
+    return jsonify(f"{image_id}, {tag_name}")
+
+
+@app.route("/tag")
+def get_tags():
+    return jsonify(db.get_tags())
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if args.init:
+        if args.tags is None or args.image_root is None:
+            raise Exception(f"require IMAGE_ROOT and TAGS with --init")
+        if args.db_path:
+            db_path = args.db_path
+        else:
+            db_path = os.path.join(args.image_root, DEFAULT_DB_NAME)
+
+        db = dba.DBAdapter(db_path)
+        if args.force:
+            db.drop_db()
+        db.init_db(args.image_root, args.image_exts, args.tags)
+    else:
+        db = dba.DBAdapter(args.db_path)
+
+    init_flask_admin()
+    app.run(host=args.ip, port=args.port, debug=args.debug)
---
+++ sqlite_adapter.py
@@ -0,0 +1,133 @@
+import json
+
+import os
+from typing import Type, List
+
+from peewee import *
+
+DB = SqliteDatabase(None)
+
+
+class DatasetInfo(Model):
+    image_root = CharField()
+    image_exts = CharField()
+
+    class Meta:
+        database = DB
+
+
+class Image(Model):
+    name = CharField(max_length=255, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        database = DB
+
+
+class Tag(Model):
+    name = CharField(max_length=255, unique=True)
+    super_name = CharField()
+
+    def __unicode__(self):
+        return f"{self.super_name}->{self.name}"
+
+    class Meta:
+        database = DB
+
+
+class ImageToTag(Model):
+    image = ForeignKeyField(Image)
+    tag = ForeignKeyField(Tag)
+
+    class Meta:
+        database = DB
+
+
+class DBAdapter(object):
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        DB.init(self.db_path)
+
+    def init_db(self, image_root: str, image_exts: str, tags_path: str):
+        DB.create_tables([DatasetInfo, Image, Tag, ImageToTag], safe=False)
+
+        dataset = DatasetInfo.create(image_root=image_root, image_exts=image_exts)
+        dataset.save()
+
+        self.update_images(image_root, image_exts)
+        self.init_tags(tags_path)
+
+    def drop_db(self):
+        DB.drop_tables([DatasetInfo, Image, Tag, ImageToTag])
+
+    def init_tags(self, tags_path: str):
+        with open(tags_path) as f:
+            tags = json.load(f)
+        self.insert_many_atomic(Tag, tags)
+
+    def update_images(self, image_root: str, image_exts: str):
+        print(f"searching image files {image_exts} in {image_root}")
+
+        images = os.listdir(image_root)
+        exts = image_exts.split(",")
+
+        def is_valid_image(file_name):
+            full_path = os.path.join(image_root, file_name)
+            lower_ext = os.path.splitext(file_name)[1].lower()
+            return os.path.exists(full_path) and lower_ext in exts
+
+        images_in_dir = set([f for f in images if is_valid_image(f)])
+
+        print(f"{len(images_in_dir)} files found")
+
+        images_in_db = set([i.name for i in Image.select()])
+        print(f"{len(images_in_db)} records found in db {self.db_path}")
+
+        new_images = sorted(list(images_in_dir - images_in_db))
+        print(f"insert {len(new_images)} images into db")
+        self.insert_many_atomic(Image, [{"name": n} for n in new_images])
+
+    def insert_many_atomic(self, model: Type[Model], data: List[dict]):
+        with DB.atomic():
+            for i in range(0, len(data), 100):
+                model.insert_many(data[i:i + 100]).execute()
+
+    def get_dataset_info(self) -> DatasetInfo:
+        return DatasetInfo.select().limit(1)[0]
+
+    def get_image_name(self, image_id):
+        return Image.get(Image.id == image_id).name
+
+    def get_image_count(self):
+        return Image.select().count()
+
+    def get_images(self):
+        return Image.select()
+
+    def get_tag_by_name(self, name: str):
+        return Tag.get(Tag.name == name)
+
+    def get_tags(self):
+        ret = {}
+        for tag in Tag.select():
+            if tag.super_name in ret:
+                ret[tag.super_name].append(tag.name)
+            else:
+                ret[tag.super_name] = [tag.name]
+
+        return ret
+
+    def get_as(self, t: Type[Model], obj):
+        if type(obj) == int:
+            ret = t.get(t.id == obj)
+        else:
+            ret = obj
+        return ret
+
+    def add_tag_to_image(self, _image, _tag):
+        image = self.get_as(Image, _image)
+        tag = self.get_as(Tag, _tag)
+        rel = ImageToTag.create(image=image, tag=tag)
+        rel.save()
